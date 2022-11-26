073c95a75eebed4c88639f15a121bef247a002af
Andi Huber <ahuber@apache.org>
2022-11-26 09:00:44 +0100

    initial commons: adding metric unit utils

---
+++ commons/src/main/java/at/ac/univie/nutrition/dita/commons/MetricUnits.java
@@ -0,0 +1,109 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package at.ac.univie.nutrition.dita.commons;
+
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.Serializable;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+import javax.measure.MetricPrefix;
+import javax.measure.Quantity;
+import javax.measure.Quantity.Scale;
+import javax.measure.Unit;
+import javax.measure.format.UnitFormat;
+import javax.measure.quantity.Dimensionless;
+import javax.measure.quantity.Mass;
+import javax.measure.quantity.Volume;
+
+import lombok.SneakyThrows;
+import lombok.val;
+import lombok.experimental.UtilityClass;
+import tech.units.indriya.AbstractUnit;
+import tech.units.indriya.format.EBNFUnitFormat;
+import tech.units.indriya.quantity.Quantities;
+import tech.units.indriya.unit.Units;
+
+@UtilityClass
+public class MetricUnits {
+
+    // -- FACTORIES
+
+    public Quantity<Dimensionless> one() {
+        return Quantities.getQuantity(1, AbstractUnit.ONE);
+    }
+
+    public Quantity<Mass> grams(final Number amount) {
+        return Quantities.getQuantity(amount, Units.GRAM);
+    }
+
+    public Quantity<Mass> milliGrams(final Number amount) {
+        return Quantities.getQuantity(amount, MetricPrefix.MILLI(Units.GRAM));
+    }
+
+    public Quantity<Volume> litres(final Number amount) {
+        return Quantities.getQuantity(amount, Units.LITRE);
+    }
+
+    public Quantity<Volume> milliLitres(final Number amount) {
+        return Quantities.getQuantity(amount, MetricPrefix.MILLI(Units.LITRE));
+    }
+
+
+    // -- FORMATTING
+
+    private final DecimalFormat decimal3Format =
+            new DecimalFormat("##0.000",DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+    private final UnitFormat unitFormat = EBNFUnitFormat.getInstance();
+
+    public String formatted(final Quantity<?> quantity) {
+        return formatted(quantity.getValue()) + formatted(quantity.getUnit());
+    }
+
+    public String formatted(final Number value) {
+        return decimal3Format.format(value);
+    }
+
+    /** SI prefixed unit symbol */
+    public String formatted(final Unit<?> unit) {
+        return unitFormat.format(unit);
+    }
+
+    // -- SERIALIZATION
+
+    /** assuming used {@link Unit} is {@link Serializable} */
+    @SneakyThrows
+    public void serialize(final Quantity<?> quantity, final ObjectOutput out) {
+        out.writeObject(quantity.getValue());
+        out.writeObject(quantity.getScale());
+        out.writeObject(quantity.getUnit());
+    }
+
+    /** assuming used {@link Unit} is {@link Serializable} */
+    @SneakyThrows
+    public Quantity<?> deserialize(final ObjectInput in) {
+        val amount = (Number)in.readObject();
+        val scale = (Scale)in.readObject();
+        val unit = (Unit)in.readObject();
+        return Quantities.getQuantity(amount, unit, scale);
+    }
+
+}
---
+++ commons/src/test/java/at/ac/univie/nutrition/dita/commons/MetricUnitsTest.java
@@ -0,0 +1,102 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package at.ac.univie.nutrition.dita.commons;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.Serializable;
+
+import javax.measure.Quantity;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.causeway.commons.internal.base._Casts;
+import org.apache.causeway.commons.internal.resources._Serializables;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.val;
+import lombok.experimental.Accessors;
+
+class MetricUnitsTest {
+
+    @RequiredArgsConstructor
+    @Getter @Accessors(fluent=true)
+    enum Scenario {
+        ONE(MetricUnits.one(), "1.000"),
+        G(MetricUnits.grams(Math.PI), "3.142g"),
+        L(MetricUnits.litres(Math.PI), "3.142l"),
+        MILLI_G(MetricUnits.milliGrams(Math.PI), "3.142mg"),
+        MILLI_L(MetricUnits.milliLitres(Math.PI), "3.142ml"),
+        ;
+        final Quantity<?> quantity;
+        final String expectedFormat;
+        void assertValidFormat() {
+            assertEquals(expectedFormat(), MetricUnits.formatted(quantity()));
+        }
+        void assertValidSerialization() {
+            val holder = new QuantityHolder(quantity());
+            assertEquals(holder, roundtrip(holder));
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource
+    void testFormats(final Scenario scenario) {
+        scenario.assertValidFormat();
+    }
+
+    @ParameterizedTest
+    @EnumSource
+    void testSerialization(final Scenario scenario) {
+        scenario.assertValidSerialization();
+    }
+
+    // -- HELPER
+
+    @Data @AllArgsConstructor @NoArgsConstructor
+    private static class QuantityHolder implements Externalizable {
+        private Quantity<?> quantity;
+        @Override
+        public void writeExternal(final ObjectOutput out) throws IOException {
+            MetricUnits.serialize(quantity, out);
+        }
+        @Override
+        public void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
+            quantity = MetricUnits.deserialize(in);
+        }
+    }
+
+    @SneakyThrows
+    private static <T extends Serializable> T roundtrip(final T object) {
+        val bytes = _Serializables.write(object);
+        return _Casts.uncheckedCast(
+                _Serializables.read(object.getClass(), bytes));
+    }
+
+}
