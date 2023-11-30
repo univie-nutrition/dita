6440143e5431c108df0f9d64ded9c7f7d35e8557
andi-huber <ahuber@apache.org>
2023-11-30 11:40:01 +0100

    help pages: table page, adds icons and sections

--- globodiet/manager/src/main/java/dita/globodiet/manager/help/DitaTableNamesPage.java
+++ globodiet/manager/src/main/java/dita/globodiet/manager/help/DitaTableNamesPage.java
@@ -18,14 +18,24 @@
  */
 package dita.globodiet.manager.help;
 
+import java.util.Optional;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
+import org.asciidoctor.ast.Table;
 import org.springframework.stereotype.Component;
 
+import org.apache.causeway.applib.fa.FontAwesomeLayers;
+import org.apache.causeway.core.metamodel.context.MetaModelContext;
+import org.apache.causeway.core.metamodel.facets.members.iconfa.FaFacet;
+import org.apache.causeway.core.metamodel.facets.members.iconfa.FaLayersProvider;
 import org.apache.causeway.extensions.docgen.help.applib.HelpPage;
 import org.apache.causeway.valuetypes.asciidoc.applib.value.AsciiDoc;
 import org.apache.causeway.valuetypes.asciidoc.builder.AsciiDocBuilder;
+import org.apache.causeway.valuetypes.asciidoc.builder.AsciiDocFactory;
 
 import static org.apache.causeway.valuetypes.asciidoc.builder.AsciiDocFactory.cell;
 import static org.apache.causeway.valuetypes.asciidoc.builder.AsciiDocFactory.headCell;
@@ -41,6 +51,7 @@ import lombok.val;
 public class DitaTableNamesPage implements HelpPage {
 
     @Inject OrmModel.Schema gdParamsSchema;
+    @Inject MetaModelContext mmc;
 
     @Override
     public String getTitle() {
@@ -49,31 +60,70 @@ public class DitaTableNamesPage implements HelpPage {
 
     @Override
     public AsciiDoc getContent() {
-
         val adoc = new AsciiDocBuilder();
+        var namespaces = gdParamsSchema.entities().values().stream()
+            .map(e->e.namespace())
+            .collect(Collectors.toCollection(TreeSet::new));
+
         adoc.append(doc->{
-            doc.setTitle("Tables (UI vs Database)");
+            var section = AsciiDocFactory.section(doc, "Tables (UI vs Database) grouped by Namespace");
 
-            var table = table(doc);
-            table.setAttribute("cols", "2m,1m,4m,2m", true);
+            var table = table(section);
+            table.setAttribute("cols", "^1a,3a,2m,8m", true);
+            headCell(table, 0, 0, "Icon");
+            headCell(table, 0, 1, "UI");
+            headCell(table, 0, 2, "Database");
+            headCell(table, 0, 3, "Description");
 
-            headCell(table, 0, 0, "UI");
-            headCell(table, 0, 1, "Database");
-            headCell(table, 0, 2, "Description");
-            headCell(table, 0, 3, "Category (Namespace)");
-
-            gdParamsSchema.entities().values()
-            .stream()
-            .sorted((a, b)->a.name().compareTo(b.name()))
-            .forEach(t->{
-                var row = row(table);
-                cell(table, row, t.name());
-                cell(table, row, t.table());
-                cell(table, row, t.formatDescription("\n"));
-                cell(table, row, t.namespace());
-            });
+            namespaces.forEach(ns->appendNamespaceSection(table, ns));
         });
+
         return adoc.buildAsValue();
+    }
+
+    private void appendNamespaceSection(final Table table, final String namespace) {
+
+        { // section header within table
+            var row = row(table);
+            cell(table, row, "");
+            cell(table, row, namespace.substring("params".length() + 1).toUpperCase());
+            cell(table, row, "");
+            cell(table, row, "");
+        }
+
+        gdParamsSchema.entities().values().stream()
+        .filter(e->e.namespace().equalsIgnoreCase(namespace))
+        .sorted((a, b)->a.name().compareTo(b.name()))
+        .forEach(t->{
+            var row = row(table);
+            var logicalName = "dita.globodiet."  + t.namespace() + "." + t.name();
+            cell(table, row, htmlPassThroughIconFor(logicalName));
+            cell(table, row, "`" + t.name() + "`");
+            cell(table, row, t.table());
+            cell(table, row, t.formatDescription("\n"));
+        });
+
+    }
+
+    // -- HELPER
+
+    private String htmlPassThroughIconFor(final String logicalName) {
+        return faIcon(logicalName)
+                .map(FontAwesomeLayers::toHtml)
+                .map(faHtml->String.format("""
+                        ++++
+                        %s
+                        ++++
+                        """, faHtml))
+                .orElse("");
+    }
+
+    private Optional<FontAwesomeLayers> faIcon(final String logicalName) {
+        return mmc.getSpecificationLoader().specForLogicalTypeName(logicalName)
+                .map(spec->spec.getFacet(FaFacet.class))
+                .filter(FaLayersProvider.class::isInstance)
+                .map(FaLayersProvider.class::cast)
+                .map(FaLayersProvider::getLayers);
     }
 
 }
