85db0befc4db53faf4c1b20c243c276c964fdd11
andi-huber <ahuber@apache.org>
2024-03-24 20:27:24 +0100

    highlight_dependants fix (don't highlight if already highlighted)

--- globodiet/manager/src/main/resources/static/scripts/application.js
+++ globodiet/manager/src/main/resources/static/scripts/application.js
@@ -2,6 +2,7 @@ function highlight_dependants() {
 	var spans = Array.from(document.getElementsByClassName('card-title'));
 	spans.forEach(span => {
 	   var highlighted = span.innerHTML;
+	   if(highlighted.includes("<b>")) return;
 	   highlighted = highlighted.replace("Dependent ", "Dependent <b>")
 	   highlighted = highlighted.replace(" Mapped By ", "</b> mapped by <b>") + "</b>" 
 	   span.innerHTML = highlighted;
