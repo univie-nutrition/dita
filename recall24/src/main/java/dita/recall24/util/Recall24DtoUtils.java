01ec0198c7e6e1f80a43f12e6979f72c3405afcd
andi-huber <ahuber@apache.org>
2024-05-31 18:35:16 +0200

    reinstate correction transformer

--- recall24/src/main/java/dita/recall24/util/Recall24DtoUtils.java
+++ recall24/src/main/java/dita/recall24/util/Recall24DtoUtils.java
@@ -128,8 +128,6 @@ public class Recall24DtoUtils {
     public UnaryOperator<InterviewSet24.Dto> transform(
             final @NonNull RecallNode24.Transfomer transformer) {
         
-        return (final InterviewSet24.Dto interviewSet) -> interviewSet;
-/*        
         return (final InterviewSet24.Dto interviewSet) -> {
 
             final var stack = new int[4];
@@ -213,7 +211,7 @@ public class Recall24DtoUtils {
             var transformedInterviewSet = helper.build(setBuilder);
             return transformedInterviewSet;
             
-        };*/
+        };
     }
 
     public UnaryOperator<InterviewSet24.Dto> correct(final @Nullable Correction24 correction24) {
